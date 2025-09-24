a294c2fff95318a0946a093dee5c21ef9f4d6192
Jhordy-Marcelo <n00469317@upn.pe>
2025-09-24 02:17:57 -0500

    Agregando codigo del ejercicio 1 en la carpeta de condicionales dobles en Python

---
+++ EstructurasControl/Condicionales/Dobles/Python/ejercicio1.py
@@ -0,0 +1,9 @@
+print("=== Verificación de número par o impar ===")
+
+numero = int(input("Ingresa un número: "))
+
+# Condicional doble
+if numero % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
