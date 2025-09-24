21672aaeb81e0feccaead875c85ffda810ef95cb
Jhordy-Marcelo <n00469317@upn.pe>
2025-09-24 04:21:08 -0500

    Agregando el codigo del ejercicio 1 de Csharp en Repetitivas For

---
+++ EstructurasControl/Repetitivas/For/Csharp/ejercicio1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class RepetitivaFor1
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Números del 1 al 10 ===");
+
+        // Bucle for
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
