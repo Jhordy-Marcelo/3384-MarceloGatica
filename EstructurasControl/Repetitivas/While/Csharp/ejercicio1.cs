60e7650f1f5eb7013ad8f4146a463839ef072378
Jhordy-Marcelo <n00469317@upn.pe>
2025-09-24 04:41:14 -0500

    Agregando el codigo del ejercicio 1 de Csharp en Repetitivas While

---
+++ EstructurasControl/Repetitivas/While/Csharp/ejercicio1.cs
@@ -0,0 +1,18 @@
+using System;
+
+class RepetitivaWhile1
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Contar del 1 al 5 ===");
+
+        int i = 1;
+
+        // Bucle while
+        while (i <= 5)
+        {
+            Console.WriteLine(i);
+            i++; // incremento del contador
+        }
+    }
+}
